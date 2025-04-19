43176c3f98d9d018f28b4c824baaba2aa38b6a52
comame <dev@comame.xyz>
2025-04-19 23:16:38 +0900

    テストケース再構成

--- typed_spec.rb
+++ typed_spec.rb
@@ -3,49 +3,39 @@
 require 'rspec'
 require './typed'
 
+class StructA < TypedStruct
+  define :n, :int
+  define :str, :string
+end
+
+class StructB < TypedStruct
+  define :n, :int
+end
+
 describe 'TypedStruct' do
   it 'プリミティブ型の初期値を未指定' do
-    c = Class.new TypedStruct do
-      define :n, :int
-      define :str, :string
-    end
-
-    v = c.new
+    v = StructA.new
 
     expect(v.n).to eq 0
     expect(v.str).to eq ''
   end
 
   it 'プリミティブ型の初期値を指定した場合' do
-    c = Class.new TypedStruct do
-      define :n, :int
-      define :str, :string
-    end
-
-    v = c.new n: 53, str: 'Hello, world!'
+    v = StructA.new n: 53, str: 'Hello, world!'
 
     expect(v.n).to eq 53
     expect(v.str).to eq 'Hello, world!'
   end
 
   it 'プリミティブ型の初期値を部分的に指定した場合' do
-    c = Class.new TypedStruct do
-      define :n, :int
-      define :str, :string
-    end
-
-    v = c.new n: 53
+    v = StructA.new n: 53
 
     expect(v.n).to eq 53
     expect(v.str).to eq ''
   end
 
   it 'safe_assign で代入できる' do
-    c = Class.new TypedStruct do
-      define :n, :int
-    end
-
-    v = c.new
+    v = StructB.new
 
     ok = v.safe_assign :n, 3
     expect(ok).to be true
@@ -57,11 +47,7 @@ describe 'TypedStruct' do
   end
 
   it '=で代入できる' do
-    c = Class.new TypedStruct do
-      define :n, :int
-    end
-
-    v = c.new
+    v = StructB.new
 
     v.n = 3
     expect(v.n).to eq 3
@@ -70,11 +56,7 @@ describe 'TypedStruct' do
   end
 
   it '初期化時に値を指定できる' do
-    c = Class.new TypedStruct do
-      define :n, :int
-    end
-    expect(c.new(n: 3).n).to eq 3
-
-    expect { c.new(n: '10') }.to raise_error TypeError
+    expect(StructB.new(n: 3).n).to eq 3
+    expect { StructB.new(n: '10') }.to raise_error TypeError
   end
 end
